acb0929cb15a89598cd883b6df6c90a352cc5a89
alomgir <alomgir.hossain.official.dev@gmail.com>
2026-09-12 18:18:56 +0600

    user login controller complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,11 +19,11 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = new userModel({name, email, password: hashedPassword});
+        const user = new userModel({ name, email, password: hashedPassword });
 
         await user.save();
 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECREAT, {expiresIn: "7d"});
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECREAT, { expiresIn: "7d" });
 
         res.cookie("token", token, {
             httpOnly: true,
@@ -31,8 +31,46 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
-        
+
+        return res.json({ success: true });
+
     } catch (error) {
-        res.json({ success: false, message: error.message })
+        return res.json({ success: false, message: error.message })
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({ success: false, message: "Email and Passward are required" });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: "Invalid email" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid password" })
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECREAT, { expiresIn: "7d" });
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ success: true });
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
     }
 }
